15a20bfb2a2c8304b4848a7aadbe055423112a7d
ngpalma <nicolasgerardopalma@gmail.com>
2023-06-25 02:47:30 -0300

    landing-styles-changes

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,7 +1,18 @@
 import { Link } from "react-router-dom";
 import styles from "./Landing.module.css";
+import { useState } from "react";
 
 const Landing = () => {
+  const [showCaption, setShowCaption] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowCaption(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowCaption(false);
+  };
+
   return (
     <div className={styles.ppalContainer}>
       <h1>APLICACIÓN DE PAÍSES</h1>
@@ -10,24 +21,35 @@ const Landing = () => {
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Globo_terraqueo_3.gif"
           alt="Mapa mundi"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
         />
       </Link>
+      {showCaption && (
+        <div className={styles.caption}>
+          <h3>↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑</h3>
+          <h3>INGRESAR AL SITIO</h3>
+        </div>
+      )}
       <div className={styles.linksDiv}>
         <h2>Por Nicolás Palma</h2>
-        <Link to="https://www.linkedin.com/in/nicol%C3%A1s-palma-137541b0/" target="_blank">
+        <Link
+          to="https://www.linkedin.com/in/nicol%C3%A1s-palma-137541b0/"
+          target="_blank"
+        >
           <img
             src="https://www.pngplay.com/wp-content/uploads/12/LinkedIn-No-Background.png"
             alt="LinkedIn"
           />
         </Link>
         <Link to="https://github.com/ngpalma" target="_blank">
-          <img
-            src="https://pngimg.com/d/github_PNG83.png"
-            alt="GitHub"
-          />
+          <img src="https://pngimg.com/d/github_PNG83.png" alt="GitHub" />
         </Link>
         <Link to="mailto:nicolasgerardopalma@gmail.com" target="_blank">
-<img src="https://cdn.onlinewebfonts.com/svg/img_432958.png" alt="Gmail"/>
+          <img
+            src="https://cdn.onlinewebfonts.com/svg/img_432958.png"
+            alt="Gmail"
+          />
         </Link>
       </div>
     </div>
